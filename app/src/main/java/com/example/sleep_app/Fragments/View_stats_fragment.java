fd4656e9eb1f93eb5225eb7a1b9464c23f79dee6
nickwouters <nick.wouters@vub.be>
2020-11-15 23:11:48 +0100

    Scrollview

--- app/src/main/java/com/example/sleep_app/Fragments/View_stats_fragment.java
+++ app/src/main/java/com/example/sleep_app/Fragments/View_stats_fragment.java
@@ -1,12 +1,18 @@
 package com.example.sleep_app.Fragments;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.sleep_app.MyDBHandler;
 import com.example.sleep_app.R;
@@ -68,12 +74,71 @@ public class View_stats_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_stats_fragment, container, false);
+        View v = inflater.inflate(R.layout.fragment_view_stats_fragment, container, false);
+        LinearLayout linearLayout = v.findViewById(R.id.layoutscroll);
+        //LinearLayout layout = (LinearLayout) findViewById(R.id.info);
+
+        MyDBHandler dbHandler = new MyDBHandler(getActivity());
+        String sleep_table = dbHandler.loadSleepHandler();
+        String[] lines = sleep_table.split(System.getProperty("line.separator"));
+        int count = Integer.parseInt(lines[0]);
+
+
+        for (int i=1; i<count+1; i++){
+            String[] data = lines[i].split(" ");
+
+
+            View divider = new View(getActivity());
+            divider.setLayoutParams(new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    1
+            ));
+            divider.setBackgroundColor(Color.parseColor("#000000"));
+
+            Button btn = new Button((getActivity()));
+            btn.setText(R.string.this_night);
+            btn.setId(i-1);
+            int sleep_id = Integer.parseInt(data[0]);
+            btn.setOnClickListener(new View.OnClickListener() {
+
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void onClick(View v) {
+                    ShowNightGraph(sleep_id, dbHandler);
+                }
+            });
+
+            TextView test = new TextView(getActivity());
+            test.setText(String.format("%s%s", getString(R.string.start_str), data[1]));
+            test.setId(i-1);
+            test.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.FILL_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            ((LinearLayout) linearLayout).addView(test);
+            ((LinearLayout) linearLayout).addView(btn);
+            ((LinearLayout) linearLayout).addView(divider);
+        }
+        return v;
     }
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
-        GraphView graph = (GraphView) getView().findViewById(R.id.graph);
+        MyDBHandler dbHandler = new MyDBHandler(getActivity());
+        int sleep_id = dbHandler.GetLastSleepId();
+        sleep_id=3; //Voor tests
+        ShowNightGraph(sleep_id, dbHandler);
 
+
+        //graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
+        //graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
+        //graph.getGridLabelRenderer().setHumanRounding(false);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void ShowNightGraph(int sleep_id, MyDBHandler dbHandler){
+        GraphView graph = (GraphView) getView().findViewById(R.id.graph);
+        graph.removeAllSeries();
         // activate horizontal zooming and scrolling
         graph.getViewport().setScalable(true);
 
@@ -86,18 +151,10 @@ public class View_stats_fragment extends Fragment {
         // activate vertical scrolling
         graph.getViewport().setScrollableY(true);
 
-
-        MyDBHandler dbHandler = new MyDBHandler(getActivity());
-        //int sleep_id = dbHandler.GetLastSleepId();
-        int sleep_id = 3;
         DataPoint[] value_time_pairs = dbHandler.getSleepData(sleep_id);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(
                 value_time_pairs
         );
         graph.addSeries(series);
-
-        //graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
-        //graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
-        //graph.getGridLabelRenderer().setHumanRounding(false);
     }
 }
